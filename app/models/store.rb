03dbaeb46f9d7ee17df84a386544759971c7ee0b
BrandonKim <brandonkim@virginia.edu>
2020-07-27 17:38:15 -0400

    Fixed syntax errors

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,7 @@
 class Store < ApplicationRecord
     validates :phone_number, :hours, :address, presence: true
 
-    has_many: :orders,
+    has_many :orders,
         primary_key: :id,
         foreign_key: :store_id,
         class_name: :Orders
